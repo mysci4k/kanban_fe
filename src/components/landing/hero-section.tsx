06f164122793e9194626398029b8b83aa5427e26
Adam Szamalek <szamalekadam@gmail.com>
2025-12-22 21:30:30 +0100

    feat: add entrance animations to HeroSection

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -1,7 +1,63 @@
+import { IconArrowUpRight } from "@tabler/icons-react";
+import { Variants } from "motion";
+import * as motion from "motion/react-client";
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
-import { IconArrowUpRight } from "@tabler/icons-react";
+
+const container = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.3, delayChildren: 0.1 },
+  },
+};
+
+const title: Variants = {
+  hidden: { opacity: 0, y: -40, filter: "blur(4px)" },
+  visible: {
+    opacity: 1,
+    y: 0,
+    filter: "blur(0px)",
+    transition: { duration: 0.6, ease: "easeOut" },
+  },
+};
+
+const description: Variants = {
+  hidden: { opacity: 0, y: 40, filter: "blur(4px)" },
+  visible: {
+    opacity: 1,
+    y: 0,
+    filter: "blur(0px)",
+    transition: { duration: 0.6, ease: "easeOut" },
+  },
+};
+
+const button: Variants = {
+  hidden: { opacity: 0, scale: 0, filter: "blur(4px)" },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    filter: "blur(0px)",
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+      type: "spring",
+      stiffness: 100,
+      damping: 10,
+    },
+  },
+};
+
+const image: Variants = {
+  hidden: { opacity: 0, x: 500, filter: "blur(4px)" },
+  visible: {
+    opacity: 1,
+    x: 0,
+    filter: "blur(0px)",
+    transition: { duration: 0.6, ease: "easeOut" },
+  },
+};
 
 export function HeroSection() {
   return (
@@ -15,28 +71,43 @@ export function HeroSection() {
         <div className="absolute top-0 left-0 h-320 w-60 -translate-y-87.5 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
       </div>
 
-      <section id="home" className="overflow-hidden">
+      <motion.section
+        variants={container}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        id="home"
+        className="overflow-hidden"
+      >
         <div className="relative mx-auto max-w-5xl px-6 pt-28 lg:pt-24">
           <div className="relative z-10 mx-auto max-w-2xl text-center">
-            <h1 className="text-4xl font-semibold text-balance md:text-5xl lg:text-6xl">
+            <motion.h1
+              variants={title}
+              className="text-4xl font-semibold text-balance md:text-5xl lg:text-6xl"
+            >
               Blast through tasks with Kanblast
-            </h1>
-            <p className="text-muted-foreground mx-auto my-8 max-w-2xl text-xl">
+            </motion.h1>
+            <motion.p
+              variants={description}
+              className="text-muted-foreground mx-auto my-8 max-w-2xl text-xl"
+            >
               The supercharged Kanban board that turns chaos into productivity.
               Visualize, optimize and accelerate your projects like never
               before.
-            </p>
+            </motion.p>
 
-            <Link
-              href="/signup"
-              className={buttonVariants({ variant: "default" })}
-            >
-              <IconArrowUpRight className="size-5" /> Get Started
-            </Link>
+            <motion.div variants={button}>
+              <Link
+                href="/signup"
+                className={buttonVariants({ variant: "default" })}
+              >
+                <IconArrowUpRight className="size-5" /> Get Started
+              </Link>
+            </motion.div>
           </div>
         </div>
 
-        <div className="mx-auto 2xl:max-w-7xl">
+        <motion.div variants={image} className="mx-auto 2xl:max-w-7xl">
           <div className="pl-8 perspective-distant lg:pl-44">
             <div className="rotate-x-20 skew-x-12 mask-r-from-75% mask-b-from-55% mask-b-to-100% pt-6 pl-6 lg:h-176">
               <Image
@@ -55,8 +126,8 @@ export function HeroSection() {
               />
             </div>
           </div>
-        </div>
-      </section>
+        </motion.div>
+      </motion.section>
     </>
   );
 }
