d3d1fea2580a09e62f0583933ec4d360345744df
Adam Szamalek <szamalekadam@gmail.com>
2025-12-22 20:11:18 +0100

    feat: add Navbar to Root Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/navbar/navbar";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import clsx from "clsx";
 import type { Metadata } from "next";
@@ -51,6 +52,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
