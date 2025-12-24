53fc0f13c77097525da8045c1d756624fb48945a
Adam Szamalek <szamalekadam@gmail.com>
2025-12-24 21:39:25 +0100

    refactor: use IconLoader2 in Spinner component

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,9 +1,9 @@
 import { cn } from "@/lib/utils";
-import { IconLoader } from "@tabler/icons-react";
+import { IconLoader2 } from "@tabler/icons-react";
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
-    <IconLoader
+    <IconLoader2
       role="status"
       aria-label="Loading"
       className={cn("size-4 animate-spin", className)}
