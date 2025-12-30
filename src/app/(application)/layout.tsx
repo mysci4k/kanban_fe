fe0cce41d388316dd9632919430fbe8b879bcbad
Adam Szamalek <szamalekadam@gmail.com>
2025-12-30 22:08:15 +0100

    feat: add sidebar and header to application layout

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -1,9 +1,17 @@
+import { AppHeader } from "@/shared/components/layout/header/app-header";
+import { AppSidebar } from "@/shared/components/layout/sidebar/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/shared/components/ui/sidebar";
+
 export default function AppLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-      <main>{children}</main>
-    </>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <AppHeader />
+        <main>{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
