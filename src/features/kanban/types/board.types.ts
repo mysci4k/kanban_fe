57fa5883a13f459fc5c49c61eb32ff597e5cec3a
Adam Szamalek <szamalekadam@gmail.com>
2026-01-17 13:06:18 +0100

    feat: add missing board types and interface

--- src/features/kanban/types/board.types.ts
+++ src/features/kanban/types/board.types.ts
@@ -1,11 +1,21 @@
 import { components, operations } from "@/shared/api/api-types";
+import { ColumnWithTasks } from "./column.types";
 
-// Schema types
 export type BoardDto = components["schemas"]["BoardDto"];
 export type CreateBoardDto = components["schemas"]["CreateBoardDto"];
+export type UpdateBoardDto = components["schemas"]["UpdateBoardDto"];
 
-// Response types
 export type GetUserBoardsResponse =
   operations["get_user_boards"]["responses"][200]["content"]["application/json"];
+export type GetBoardResponse =
+  operations["get_board"]["responses"][200]["content"]["application/json"];
 export type CreateBoardResponse =
   operations["create_board"]["responses"][201]["content"]["application/json"];
+export type UpdateBoardResponse =
+  operations["update_board"]["responses"][200]["content"]["application/json"];
+export type DeleteBoardResponse =
+  operations["delete_board"]["responses"][200]["content"]["application/json"];
+
+export interface BoardWithColumnsAndTasks extends BoardDto {
+  columns: ColumnWithTasks[];
+}
